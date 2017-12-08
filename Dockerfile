946b292c3f921c6e752020f85814c61c613715c5
Mahalakshmi Naidu Sanapathi <MahalakshmiNaiduSanapathi@fico.com>
2017-12-08 16:15:51 +0100

    Adding docker file and Greeting resource

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,8 @@ RUN mkdir /app
 
 WORKDIR /app
 
-COPY ../build/libs/SpringGradleProject-1.0-SNAPSHOT.jar /app/spring.jar
+COPY build/libs/SpringGradleProject*.jar /app/spring.jar
 
-COPY
 EXPOSE 8080
 
 CMD ["java", "-jar", "/app/spring.jar"]
--- src/main/java/com/smn/spring/resource/GreetingResource.java
+++ src/main/java/com/smn/spring/resource/GreetingResource.java
@@ -1,4 +1,16 @@
 package com.smn.spring.resource;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
 public class GreetingResource {
+
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return ResponseEntity.ok("Hello! " + name);
+    }
+
 }
